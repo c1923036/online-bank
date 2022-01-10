0e2f4a8e82a5684a39e664381da78f74ffc58fad
c1923036 <u1923036@live.warwick.ac.uk>
2022-01-10 20:58:06 +0000

    Updates to payment form

--- onlineBank/onlineBank/settings.py
+++ onlineBank/onlineBank/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l@=m2fu%ur(b7o*#azc!*p$0d5(0ku2)+a@i9++e424k)itfx-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["172.31.145.4"]
+ALLOWED_HOSTS = ["172.31.153.38"]
 
 
 # Application definition
