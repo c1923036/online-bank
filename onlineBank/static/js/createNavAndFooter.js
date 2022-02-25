c894217bcdce6fb18be0474fd99ba334ea6df128
c1923036 <u1923036@live.warwick.ac.uk>
2022-02-25 22:47:59 +0000

    Added comments to js script

--- onlineBank/static/js/createNavAndFooter.js
+++ onlineBank/static/js/createNavAndFooter.js
@@ -1,17 +1,17 @@
-function createNav() {
-    var navData = navbarContents
-    var footData = footerContents
-    var colour = getSyncScriptParams();
-    var nav = document.getElementById("navbar")
-    var foot = document.getElementById("footer")
+function createNav() { //Function which creates the content of the navbar
+    var navData = navbarContents;
+    var footData = footerContents;
+    var colour = getSyncScriptParams(); //Retrieves the parsed font_colour value
+    var nav = document.getElementById("navbar");
+    var foot = document.getElementById("footer");
     var strToAppend;
-    for (var i = 0; i < navData.length; i++) {
-        strToAppend = '<li class=\"nav-item\"><a class=\"nav-link\" style=\"color:' + colour + '\" href=\"' + navData[i]['url'] + '\">' + navData[i]['name'] + '</a></li>'
+    for (var i = 0; i < navData.length; i++) { //Constructs navbar innerHTML
+        strToAppend = '<li class=\"nav-item\"><a class=\"nav-link\" style=\"color:' + colour + '\" href=\"' + navData[i]['url'] + '\">' + navData[i]['name'] + '</a></li>';
         nav.innerHTML += strToAppend;
     }
-    for (var i = 0; i < footData.length; i++) {
-        strToAppend = '<li><a class="footer-Link" href="' + footData[i]['url'] + '">' + footData[i]['name'] + '</a></li>'
-        footer.innerHTML += strToAppend;
+    for (var i = 0; i < footData.length; i++) { //Contstructs footer innerHTML
+        strToAppend = '<li><a class="footer-Link" href="' + footData[i]['url'] + '">' + footData[i]['name'] + '</a></li>';
+        foot.innerHTML += strToAppend;
     }
 };
 
